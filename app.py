f54baf309bdda2af9212c96a5e03c84dd1498f89
mini2728 <mini2728@hotmail.com>
2023-10-26 09:39:54 +0800

    Update app.py
    新增測試

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def handle_message(event):
 
     # Send To Line
     reply = TextSendMessage(text=f"{get_message}")
-    line_bot_api.reply_message(event.reply_token, '別問我')
+    if reply == "機器人"
+        line_bot_api.reply_message(event.reply_token, "你好~")
